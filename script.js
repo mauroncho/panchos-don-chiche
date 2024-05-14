b09ccb4ae845e196ab381c1226829807f3d2950d
Mau! <mau.giachero@gmail.com>
2024-05-14 13:48:26 -0300

    validacion bootstrap e imagenes de sobra eliminadas

--- script.js
+++ script.js
@@ -17,66 +17,110 @@ navToggle.addEventListener("click", () => {
 
 //validacion de formulario
 
-const Formulario = document.getElementById("form");
-const inputs = document.querySelectorAll(".formulario input");
-const nombre = document.getElementById("Nombre");
-const apellido = document.getElementById("Apellido");
-const email = document.getElementById("email");
-const CodigoPostal = document.getElementById("Codigo Postal");
+// const Formulario = document.getElementById("form");
+// const inputs = document.querySelectorAll(".formulario input");
+// const nombre = document.getElementById("Nombre");
+// const apellido = document.getElementById("Apellido");
+// const email = document.getElementById("email");
+// const CodigoPostal = document.getElementById("Codigo Postal");
 
-const expresiones = {
-  Validacion: /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/,
-  ValidacionEmail:
-    /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/,
-  ValidacionCodigoPostal: /^[0-9]+$/,
-};
+// const expresiones = {
+//   Validacion: /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/,
+//   ValidacionEmail:
+//     /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/,
+//   ValidacionCodigoPostal: /^[0-9]+$/,
+// };
 
-Formulario.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+// Formulario.addEventListener("submit", (e) => {
+//   e.preventDefault();
+// });
 
-const ValidarFormulario = (e) => {
-  switch (e.target.name) {
-    case "Nombre":
-      ValidarCampo(expresiones.Validacion, e.target, "Nombre");
-      break;
-    case "Apellido":
-      ValidarCampo(expresiones.Validacion, e.target, "Apellido");
-      break;
-    case "email":
-      ValidarCampo(expresiones.ValidacionEmail, e.target, "email");
-      break;
-    case "Codigo Postal":
-      ValidarCampo(
-        expresiones.ValidacionCodigoPostal,
-        e.target,
-        "Codigo Postal"
-      );
-      break;
-  }
-};
+// const ValidarFormulario = (e) => {
+//   switch (e.target.name) {
+//     case "Nombre":
+//       ValidarCampo(expresiones.Validacion, e.target, "Nombre");
+//       break;
+//     case "Apellido":
+//       ValidarCampo(expresiones.Validacion, e.target, "Apellido");
+//       break;
+//     case "email":
+//       ValidarCampo(expresiones.ValidacionEmail, e.target, "email");
+//       break;
+//     case "Codigo Postal":
+//       ValidarCampo(
+//         expresiones.ValidacionCodigoPostal,
+//         e.target,
+//         "Codigo Postal"
+//       );
+//       break;
+//   }
+// };
 
-const ValidarCampo = (expresion, input, campo) => {
-  if (!expresion.test(input.value) || !input.value.trim()) {
-    document.getElementById("error").style.display = "block";
-    document.getElementById("aceptado").style.display = "none";
-  } else {
-    document.getElementById("aceptado").style.display = "block";
-    document.getElementById("error").style.display = "none";
-  }
-};
+// const ValidarCampo = (expresion, input, campo) => {
+//   if (!expresion.test(input.value) || !input.value.trim()) {
+//     document.getElementById("error").style.visibility = "visible";
+//     document.getElementById("aceptado").style.visibility = "hidden";
+//   } else {
+//     document.getElementById("aceptado").style.visibility = "visible";
+//     document.getElementById("error").style.visibility = "hidden";
+//   }
+// };
 
-inputs.forEach((input) => {
-  input.addEventListener("keyup", ValidarFormulario);
-  input.addEventListener("blur", ValidarFormulario);
-});
+// inputs.forEach((input) => {
+//   input.addEventListener("keyup", ValidarFormulario);
+//   input.addEventListener("blur", ValidarFormulario);
+// });
 
-function MostrarError() {
-  document.getElementById("error").style.display = "block";
-  document.getElementById("aceptado").style.display = "none";
-}
+// function MostrarError() {
+//   document.getElementById("error").style.visibility = "visible";
+//   document.getElementById("aceptado").style.visibility = "hidden";
+// }
 
-function Aceptado() {
-  document.getElementById("aceptado").style.display = "block";
-  document.getElementById("error").style.display = "none";
-}
+// function Aceptado() {
+//   document.getElementById("aceptado").style.visibility = "visible";
+//   document.getElementById("error").style.visibility = "hidden";
+// }
+
+//NUEVO INTENTO DE VALIDACION DEL FORM
+// const formulario = document.getElementById("form");
+// const inputs = document.querySelectorAll(".formulario input");
+// const nombre = document.getElementById("Nombre");
+// const apellido = document.getElementById("Apellido");
+// const email = document.getElementById("email");
+// const codigoPostal = document.getElementById("Codigo Postal");
+
+// // const expresiones = {
+// //   Validacion: ,
+// //   ValidacionEmail:
+// //     /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/,
+// //   ValidacionCodigoPostal: /^[0-9]+$/,
+// // };
+
+// const validacionNombre = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+// const validacionEmail =
+//   /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2, 15})$/;
+// const validacionCodigoPostal = /^[0-9]+$/;
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
