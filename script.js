1b44363e4c9962f099a016ca1752e9ef78476404
Mau! <mau.giachero@gmail.com>
2024-05-13 22:38:36 -0300

    form mal hecho

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //recupero los elementos del dom
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.getElementById("nav-principal");
-console.log(navToggle);
+// console.log(navToggle);
 //cambio atributos cuando el usuario hace click
 navToggle.addEventListener("click", () => {
   const visibility = navMenu.getAttribute("data-visible");
@@ -17,73 +17,72 @@ navToggle.addEventListener("click", () => {
 
 //validacion de formulario
 
+let Validacion = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+let ValidacionEmail =
+  /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/;
+let ValidacionCodigoPostal = /^[0-9]+$/;
 
-let Validacion = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/
-let ValidacionEmail= /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/
-let ValidacionCodigoPostal = /^[0-9]+$/
+const Formulario = document.getElementById("form");
+const nombre = document.getElementById("Nombre");
+const apellido = document.getElementById("Apellido");
+const email = document.getElementById("email");
+const CodigoPostal = document.getElementById("Codigo Postal");
 
-
-const Formulario = document.getElementById('form')
-const nombre = document.getElementById('Nombre')
-const apellido = document.getElementById ('Apellido')
-const email = document.getElementById('email')
-const CodigoPostal = document.getElementById('Codigo Postal')
-
-Formulario.addEventListener('submit', (e) =>{
-
+Formulario.addEventListener("submit", (e) => {
   e.preventDefault();
 
- if (!Validacion.test(nombre.value) || !nombre.value.trim()){
-  console.log('formato no valido')
-  MostrarError()
-  return
- }else{
-  Aceptado()
- }
+  if (!Validacion.test(nombre.value) || !nombre.value.trim()) {
+    console.log("formato no valido");
+    MostrarError();
+    return;
+  } else {
+    Aceptado();
+  }
 
-if(!Validacion.test(apellido.value) || !apellido.value.trim()){
-  console.log('formato no valido')
-  MostrarError()
-  return
-}else{
-  Aceptado()
- }
+  if (!Validacion.test(apellido.value) || !apellido.value.trim()) {
+    console.log("formato no valido");
+    MostrarError();
+    return;
+  } else {
+    Aceptado();
+  }
 
-  if(!ValidacionEmail.test(email.value) || !email.value.trim()){
-    console.log('Formato no valido')
-    MostrarError()
-    return
-  }else{
-    Aceptado()
-   }
+  if (!ValidacionEmail.test(email.value) || !email.value.trim()) {
+    console.log("Formato no valido");
+    MostrarError();
+    return;
+  } else {
+    Aceptado();
+  }
 
-  if(!ValidacionCodigoPostal.test(CodigoPostal.value) || !CodigoPostal.value.trim()){
-    console.log('Formato no valido')
-    MostrarError()
-    return
-  }else{
-    Aceptado()
-   }
+  if (
+    !ValidacionCodigoPostal.test(CodigoPostal.value) ||
+    !CodigoPostal.value.trim()
+  ) {
+    console.log("Formato no valido");
+    MostrarError();
+    return;
+  } else {
+    Aceptado();
+  }
 
-  console.log('Formulario Enviado')
-})
+  console.log("Formulario Enviado");
+});
 
-function MostrarError(){
-  let error = document.querySelectorAll('.form-error')
- // document.querySelectorAll('.form-error').style.display = 'block';
-  let aceptada = document.querySelectorAll('.form-aceptado')
-  console.log (error , aceptada)
+function MostrarError(inputId) {
+  let selectedInput = document.getElementById(inputId);
+  if (!Validacion.test(nombre.value) || !nombre.value.trim()) {
+    selectedInput.classList.add("form-error");
+    console.log("formato no valido");
+    // MostrarError();
+    return;
+  } else {
+    selectedInput.classList.add("form-aceptado");
+    // Aceptado();
+  }
+  // document.getElementById('error').style.display = 'block';
 }
 
-function Aceptado(){
-  let aceptado = document.querySelectorAll('.form-aceptado').style.display = 'block';
-  //document.querySelectorAll('.form-aceptado').style.display = 'block';
-  document.querySelectorAll('.form-error').style.display = 'none';
+function Aceptado() {
+  document.getElementById("aceptado").style.display = "block";
 }
-
-
-
-
-
-
-
