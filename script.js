9d51cdcc7736413be23defa07b7c1b4d269cfefe
Mau! <mau.giachero@gmail.com>
2024-05-14 20:16:01 -0300

    validacion form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//interaccion del menu en mobile
+//MENU DE NAVEGACIÓN RESPONSIVE
 //recupero los elementos del dom
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.getElementById("nav-principal");
@@ -15,112 +15,57 @@ navToggle.addEventListener("click", () => {
   }
 });
 
-//validacion de formulario
-
-// const Formulario = document.getElementById("form");
-// const inputs = document.querySelectorAll(".formulario input");
-// const nombre = document.getElementById("Nombre");
-// const apellido = document.getElementById("Apellido");
-// const email = document.getElementById("email");
-// const CodigoPostal = document.getElementById("Codigo Postal");
-
-// const expresiones = {
-//   Validacion: /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/,
-//   ValidacionEmail:
-//     /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/,
-//   ValidacionCodigoPostal: /^[0-9]+$/,
-// };
-
-// Formulario.addEventListener("submit", (e) => {
-//   e.preventDefault();
-// });
-
-// const ValidarFormulario = (e) => {
-//   switch (e.target.name) {
-//     case "Nombre":
-//       ValidarCampo(expresiones.Validacion, e.target, "Nombre");
-//       break;
-//     case "Apellido":
-//       ValidarCampo(expresiones.Validacion, e.target, "Apellido");
-//       break;
-//     case "email":
-//       ValidarCampo(expresiones.ValidacionEmail, e.target, "email");
-//       break;
-//     case "Codigo Postal":
-//       ValidarCampo(
-//         expresiones.ValidacionCodigoPostal,
-//         e.target,
-//         "Codigo Postal"
-//       );
-//       break;
-//   }
-// };
-
-// const ValidarCampo = (expresion, input, campo) => {
-//   if (!expresion.test(input.value) || !input.value.trim()) {
-//     document.getElementById("error").style.visibility = "visible";
-//     document.getElementById("aceptado").style.visibility = "hidden";
-//   } else {
-//     document.getElementById("aceptado").style.visibility = "visible";
-//     document.getElementById("error").style.visibility = "hidden";
-//   }
-// };
-
-// inputs.forEach((input) => {
-//   input.addEventListener("keyup", ValidarFormulario);
-//   input.addEventListener("blur", ValidarFormulario);
-// });
-
-// function MostrarError() {
-//   document.getElementById("error").style.visibility = "visible";
-//   document.getElementById("aceptado").style.visibility = "hidden";
-// }
-
-// function Aceptado() {
-//   document.getElementById("aceptado").style.visibility = "visible";
-//   document.getElementById("error").style.visibility = "hidden";
-// }
-
-//NUEVO INTENTO DE VALIDACION DEL FORM
-// const formulario = document.getElementById("form");
-// const inputs = document.querySelectorAll(".formulario input");
-// const nombre = document.getElementById("Nombre");
-// const apellido = document.getElementById("Apellido");
-// const email = document.getElementById("email");
-// const codigoPostal = document.getElementById("Codigo Postal");
-
-// // const expresiones = {
-// //   Validacion: ,
-// //   ValidacionEmail:
-// //     /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$/,
-// //   ValidacionCodigoPostal: /^[0-9]+$/,
-// // };
-
+//VALIDACIÓN FORMULARIO
 // const validacionNombre = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
 // const validacionEmail =
 //   /^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2, 15})$/;
 // const validacionCodigoPostal = /^[0-9]+$/;
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  "use strict";
+const btnEnviar = document.getElementById("form-submit");
+const contactoForm = document.getElementById("form");
+const userName = document.getElementById("floatingName");
+const userLastName = document.getElementById("floatingApellido");
+const userMail = document.getElementById("floatingEmail");
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll(".needs-validation");
+btnEnviar.addEventListener("click", () => {
+  if (userName.value == 0) {
+    userName.classList.add("is-invalid");
+    alert("Nombre es obligatorio");
+    userName.focus();
+    return false;
+  } else {
+    userName.classList.remove("is-invalid");
+    userName.classList.add("is-valid");
+  }
+  // Validar apellido
+  if (userLastName.value == "") {
+    userLastName.classList.add("is-invalid");
+    alert("Apellido es obligatorio");
+    document.formulario.apellido.focus();
+    return false;
+  } else {
+    userLastName.classList.remove("is-invalid");
+    userLastName.classList.add("is-valid");
+  }
 
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form) => {
-    form.addEventListener(
-      "submit",
-      (event) => {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
+  // Validar email
+  const regexEmail =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
 
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
+  if (!regexEmail.test(userMail.value)) {
+    userMail.classList.add("is-invalid");
+    alert("Email debe contener un email");
+    return false;
+  } else {
+    userMail.classList.remove("is-invalid");
+    userMail.classList.add("is-valid");
+  }
+
+  // Enviar formulario
+  // contactoForm.submit();
+  contactoForm.reset();
+  userName.classList.remove("is-valid");
+  userLastName.classList.remove("is-valid");
+  userMail.classList.remove("is-valid");
+  alert("Muchas gracias por enviar el formulario");
+});
